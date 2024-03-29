29e140e17cdbd4ce1de9021c2bc1644fad246178
Mark-André Hopf <mhopf@mark13.org>
2024-03-29 15:35:56 +0100

    cc_lib: add test and begin own api in namespace mediapipe::cc_lib mimicing mediapipe::tasks

---
+++ cc_lib/mediapipe_detail.hh
@@ -0,0 +1,21 @@
+#ifndef MEDIAPIPE_CCLIB_DETAIL_H_
+#define MEDIAPIPE_CCLIB_DETAIL_H_
+
+#include "mediapipe.hh"
+
+#include "mediapipe/tasks/cc/vision/core/running_mode.h"
+#include "mediapipe/tasks/cc/vision/face_landmarker/face_landmarker.h"
+#include "mediapipe/tasks/cc/vision/face_landmarker/face_landmarker_result.h"
+
+namespace mediapipe {
+namespace cc_lib {
+namespace detail {
+
+std::unique_ptr<mediapipe::tasks::vision::face_landmarker::FaceLandmarkerOptions>
+convert(const std::unique_ptr<mediapipe::cc_lib::vision::face_landmarker::FaceLandmarkerOptions> &in);
+
+}
+} // namespace cc_lib
+} // namespace mediapipe
+
+#endif
