67f28c5ad5a771632913264dd7fec4b1675dd5ba
Mark-André Hopf <mhopf@mark13.org>
2024-03-30 13:03:21 +0100

    cc_lib: use doxygen style comments in header file

--- cc_lib/mediapipe.hh
+++ cc_lib/mediapipe.hh
@@ -41,7 +41,8 @@ struct NormalizedLandmark {
     float x;
     float y;
     float z;
-    // not set by face_landmarker_v2_with_blendshapes.task
+
+    // not set by face_landmarker_v2_with_blendshapes.task, hence skipped
     // std::optional<float> visibility = std::nullopt;
     // std::optional<float> presence = std::nullopt;
     // std::optional<std::string> name = std::nullopt;
@@ -58,13 +59,19 @@ namespace core {
  *  The running mode of a MediaPipe vision task.
  */
 enum RunningMode {
-  // Run the vision task on a single image input.
+  /**
+   * Run the vision task on a single image input.
+   */
   IMAGE = 1,
 
-  // Run the vision task on the decoded frames of an input video.
+  /**
+   * Run the vision task on the decoded frames of an input video.
+   */
   VIDEO = 2,
 
-  // Run the vision task on a live stream of input data, such as from camera.
+  /**
+   * Run the vision task on a live stream of input data, such as from camera.
+   */
   LIVE_STREAM = 3,
 };
 
@@ -79,7 +86,9 @@ namespace face_landmarker {
  * element represents a single face detected in the image.
  */
 struct FaceLandmarkerResult {
-  // Detected face landmarks in normalized image coordinates.
+  /**
+   * Detected face landmarks in normalized image coordinates.
+   */
   std::vector<::mediapipe::cc_lib::components::containers::NormalizedLandmarks> face_landmarks;
 //   // Optional face blendshapes results.
 //   std::optional<std::vector<components::containers::Classifications>>
@@ -89,52 +98,71 @@ struct FaceLandmarkerResult {
 };
 
 // wrapper for mediapipe/tasks/cc/vision/face_landmarker/face_landmarker.h:FaceLandmarkerOptions
+
+/**
+ * Configuration options for FaceLandmarker
+ */
 struct FaceLandmarkerOptions {
     FaceLandmarkerOptions();
 
-    // Base options for configuring MediaPipe Tasks library, such as specifying
-    // the TfLite model bundle file with metadata, accelerator options, op
-    // resolver, etc.
+    /**
+     * Base options for configuring MediaPipe Tasks library, such as specifying
+     * the TfLite model bundle file with metadata, accelerator options, op
+     * resolver, etc.
+     */
     ::mediapipe::cc_lib::core::BaseOptions base_options;
 
-    // The running mode of the task. Default to the image mode.
-    // FaceLandmarker has three running modes:
-    // 1) The image mode for detecting face landmarks on single image inputs.
-    // 2) The video mode for detecting face landmarks on the decoded frames of a
-    //    video.
-    // 3) The live stream mode for detecting face landmarks on the live stream of
-    //    input data, such as from camera. In this mode, the "result_callback"
-    //    below must be specified to receive the detection results asynchronously.
+    /**
+     * The running mode of the task. Default to the image mode.
+     * FaceLandmarker has three running modes:
+     * 1) The image mode for detecting face landmarks on single image inputs.
+     * 2) The video mode for detecting face landmarks on the decoded frames of a
+     *    video.
+     * 3) The live stream mode for detecting face landmarks on the live stream of
+     *    input data, such as from camera. In this mode, the "result_callback"
+     *    below must be specified to receive the detection results asynchronously.
+     */
     ::mediapipe::cc_lib::vision::core::RunningMode running_mode = mediapipe::cc_lib::vision::core::RunningMode::IMAGE;
 
-    // The maximum number of faces that can be detected by the FaceLandmarker.
+    /**
+     * The maximum number of faces that can be detected by the FaceLandmarker.
+     */
     int num_faces = 1;
 
-    // The minimum confidence score for the face detection to be considered
-    // successful.
+    /**
+     * The minimum confidence score for the face detection to be considered successful.
+     */
     float min_face_detection_confidence = 0.5;
 
-    // The minimum confidence score of face presence score in the face landmark
-    // detection.
+    /**
+     * The minimum confidence score of face presence score in the face landmark detection.
+     */
     float min_face_presence_confidence = 0.5;
 
-    // The minimum confidence score for the face tracking to be considered
-    // successful.
+    /**
+     * The minimum confidence score for the face tracking to be considered successful.
+     */
     float min_tracking_confidence = 0.5;
 
-    // Whether FaceLandmarker outputs face blendshapes classification. Face
-    // blendshapes are used for rendering the 3D face model.
+    /**
+     * Whether FaceLandmarker outputs face blendshapes classification.
+     * Face blendshapes are used for rendering the 3D face model.
+     */
     bool output_face_blendshapes = false;
 
-    // Whether FaceLandmarker outputs facial transformation_matrix. Facial
-    // transformation matrix is used to transform the face landmarks in canonical
-    // face to the detected face, so that users can apply face effects on the
-    // detected landmarks.
+    /**
+     * Whether FaceLandmarker outputs facial transformation_matrix. Facial
+     * transformation matrix is used to transform the face landmarks in canonical
+     * face to the detected face, so that users can apply face effects on the
+     * detected landmarks.
+     */
     bool output_facial_transformation_matrixes = false;
 
-    // The user-defined result callback for processing live stream data.
-    // The result callback should only be specified when the running mode is set
-    // to RunningMode::LIVE_STREAM.
+    /**
+     * The user-defined result callback for processing live stream data.
+     * The result callback should only be specified when the running mode is set
+     * to RunningMode::LIVE_STREAM.
+     */
     //   std::function<void(absl::StatusOr<FaceLandmarkerResult>, const Image &,
     //                      int64_t)>
     //       result_callback = nullptr;
